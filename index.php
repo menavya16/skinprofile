b8213de1034970d02e823a6b6113b3961e661858
Ming Yang Xu <kev.xu@mail.utoronto.ca>
2019-01-19 14:26:28 -0500

    bg img

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <head>
         <meta charset = "utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        
         <title>Skin Profile</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <link href="style.css" type="text/css" rel="stylesheet">
     </head>
 
     <body id = "page-top">
        <h1 class="text-center">Welcome</h1>
 
-       <a href="register.php" class="btn btn-outline-primary">Register</a>
+       <a href="register.php" class="text-center btn btn-outline-primary" >Register</a>
     </body>
 </html>
