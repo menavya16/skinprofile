b5d203ec01ffbaa7f3db34306242c1c987b6a246
Anna J <jos5@mcmaster.ca>
2019-01-19 13:55:59 -0500

    updated login.php

--- login.php
+++ login.php
@@ -1,3 +1,30 @@
 <?php
 include "db/formhandler.php";
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+  <title> Register </title>
+</head>
+
+<body>
+  <div id="messages">
+    <?php
+      if (count($messages) > 0){
+        foreach ($messages as $e) {
+          echo "<b>".$e."</br></b>";
+        }
+      }
+    ?>
+  </div><br>
+  <form action="login.php" method="POST">
+    <label>Username (*)</label> <input type="text" name="uname_log" required/><br>
+    <label>Password (*)</label> <input type="password" name="pswd_log" required/><br>
+    <button type="submit" name="login">Login</button>
+  </form>
+</body>
+
+</html>
