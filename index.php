430c206c1c7fadf1fd9ab7c63bdfee2aa2464714
Ming Yang Xu <kev.xu@mail.utoronto.ca>
2019-01-19 16:26:01 -0500

    image to rose

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
     </head>
+
+
     <body id = "page-top">
         <div class="header">
             <h1 class="text-center">Welcome to the new You</h1>
