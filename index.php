9c95f794db8f4c2a529615cf4cc43d1ddab8f404
Ming Yang Xu <kev.xu@mail.utoronto.ca>
2019-01-19 15:03:16 -0500

    Indie Flower font

--- index.php
+++ index.php
@@ -7,15 +7,17 @@
         <title>Skin Profile</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         <link href="style.css" type="text/css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
     </head>
 
     <body id = "page-top">
         <div class="header">
-            <h1 class="text-center">Welcome</h1>
+            <h1 class="text-center">Welcome to the new You</h1>
         </div>
+
         <div class="container">
             <div class="centre col-md-4 mb-5 mb-lg-0">
-                
+
             </div>
         </div>
         <div class="container">
