4cf277c9147f93ed2eb4806d57e149c351eb4bad
Bill DuGe <billduge11@gmail.com>
2019-01-19 15:06:36 -0500

    Changed layout

--- index.php
+++ index.php
@@ -4,27 +4,45 @@
     <head>
         <meta charset = "utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
         <title>Skin Profile</title>
+
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
         <link href="style.css" type="text/css" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     </head>
-
     <body id = "page-top">
         <div class="header">
             <h1 class="text-center">Welcome</h1>
         </div>
-        <div class="container">
-            <div class="centre col-md-4 mb-5 mb-lg-0">
-                
-            </div>
-        </div>
-        <div class="container">
+        
+        <div class="container spacing">
             <div class="row">
                 <div class="col text-center">
                     <a href="register.php" class="btn btn-outline-secondary">Register</a>
                     <a href="login.php" class="btn btn-outline-secondary">Login</a>
                 </div>
             </div>
+        </div>
+
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-lg-4">
+                    <h3>Header 1</h3>
+                    <p> sample text <p>
+                </div>
+
+                <div class="col-lg-4">
+                    <h3>Header 1</h3>
+                    <p> sample text <p>
+                </div>
+
+                <div class="col-lg-4">
+                    <h3>Header 1</h3>
+                    <p> sample text <p>
+                </div>
+            </div>
+
         </div>
     </body>
 </html>
