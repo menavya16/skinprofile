ba577745bda16c415175ba7bfd0cf0acca551b0d
Bill DuGe <billduge11@gmail.com>
2019-01-20 01:43:34 -0500

    Added recommendations page

--- index.php
+++ index.php
@@ -36,20 +36,20 @@
                 <div class="col-lg-4 cursive">
                     <i class="fas fa-heart"></i>
                     <h3>Heathly Products</h3>
-                    <p class="blog">Skin profile recommends healthy skin products for all kinds of skin care needs.<p>
+                    <p class="blog">Skin profile recommends healthy skin products for all kinds of skin care needs.</p>
                 </div>
 
                 <div class="col-lg-4 cursive">
                     <i class="fas fa-code"></i>
                     <h3>Advanced Backend Technology</h3>
-                    <p class="blog"> Our code processes each users needs and produces the best products suited based on our systems algorithms <p>
+                    <p class="blog"> Our code processes each users needs and produces the best products suited based on our systems algorithms </p>
                 </div>
 
                 <div class="col-lg-4 cursive">
                     <i class="fas fa-user"></i>
                     <h3>Individualized Profile</h3>
                     <p class="blog"> When you sign up with Skin Profile, your personalized needs are stored and tracked, allowing us to continually
-                        recommend new products just for you! <p>
+                        recommend new products just for you! </p>
                 </div>
             </div>
 
